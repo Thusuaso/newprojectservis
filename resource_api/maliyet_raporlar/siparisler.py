99a50dd89f5d08c6921cd12e349330e6f01c9c99
ferdiinan <inan79@gmail.com>
2023-04-13 10:25:24 +0300

    'l'

--- resource_api/maliyet_raporlar/siparisler.py
+++ resource_api/maliyet_raporlar/siparisler.py
@@ -292,7 +292,8 @@ class SiparislerKar:
             else:
                 doviz = DovizListem()
                 dovizKur = doviz.getDovizKurListe(str(item.Yil),str(item.Ay),str(item.Gun))
-                model.kar_zarar_tl = self.__noneControl(model.odenen_try_tutar) - (self.__noneControl(model.masraf_toplam) * float(dovizKur))
+                print(dovizKur)
+                model.kar_zarar_tl = self.__noneControl(model.odenen_try_tutar) - (self.__noneControl(model.masraf_toplam) * self.__noneControl(dovizKur))
                 
             
             
